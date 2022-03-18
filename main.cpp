7cf8b28a95cb2f83409c3da153d1891e7d81b689
My Name <me@example.com>
2022-03-18 23:31:55 +0100

    Implement empty stick to pass test.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 // clear; g++ -Wall -Werror -DDBG main.cpp && ./a.out
 // clear; g++ -Wall -Werror main.cpp && ./a.out
+
+#include<string>
+#include<vector>
+#include<iostream>
+
 #define EXPECT(A) {std::cout << (std::string)((A) ? "PASSED" : "\t\tFAILED") + ": " << #A << std::endl;}
 struct Stick {
+	Stick() {};
+	std::vector<int> d_pcs{};
 };
 
 int main() {
 	{
-		Stick stick{0};
-		EXPECT((Stick{0}, {0}));
+		Stick stick;
+		EXPECT((stick.d_pcs.size() == 0));
 	}
 	return 0;
 }
