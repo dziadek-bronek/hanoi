cc3a99ab28b67c64c027d521bb7ff6620e06b15e
My Name <me@example.com>
2022-03-18 23:37:33 +0100

    Add test stick with 5 elements. Impelement stick with 5 elements.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,12 @@
 
 #define EXPECT(A) {std::cout << (std::string)((A) ? "PASSED" : "\t\tFAILED") + ": " << #A << std::endl;}
 struct Stick {
-	Stick() {};
+	Stick() = default;
+	Stick(size_t startPieces) {
+		for(size_t i = 1; i <= startPieces; ++i) {
+			d_pcs.push_back(i);
+		}
+	};
 	std::vector<int> d_pcs{};
 };
 
@@ -15,6 +20,11 @@ int main() {
 	{
 		Stick stick;
 		EXPECT((stick.d_pcs.size() == 0));
+	}
+	{
+		Stick stick{5};
+		EXPECT((stick.d_pcs.size() == 5));
+		EXPECT((stick.d_pcs.at(3) == 4));
 	}
 	return 0;
 }
