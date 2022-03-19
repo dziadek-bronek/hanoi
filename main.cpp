8dd80e246c93c1ad67352bf2ae71fcee8d39f1c3
My Name <me@example.com>
2022-03-19 01:33:57 +0100

    Refactor. Improve names, make Stick be a child of vector<int>. Minor chantes.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// clear; g++ -Wall -Werror -DDBG main.cpp && ./a.out
+// clear; g++ -Wall -Werror -DDISPLAY_STICKS main.cpp && ./a.out
 // clear; g++ -Wall -Werror main.cpp && ./a.out
 
 #include<string>
@@ -6,18 +6,18 @@
 #include<iostream>
 
 #define EXPECT(A) {std::cout << (std::string)((A) ? "PASSED" : "\t\tFAILED") + ": " << #A << std::endl;}
-struct Stick {
+
+struct Stick : std::vector<int> {
 	Stick() = default;
-	Stick(size_t startPieces) {
-		for(size_t i = startPieces; i >= 1; --i) {
-			d_pcs.push_back(i);
+	Stick(size_t startPiecesNum) {
+		for(size_t i = startPiecesNum; i >= 1; --i) {
+			push_back(i);
 		}
 	};
-	std::vector<int> d_pcs{};
-#ifdef DBG
+#ifdef DISPLAY_STICKS
 	void display() {
 		std::cout << '|';
-		for(auto i = d_pcs.begin(); i != d_pcs.end(); ++i) {
+		for(auto i = begin(); i != end(); ++i) {
 			std::cout << *i;
 		}
 		std::cout << std::endl;
@@ -28,25 +28,24 @@ struct Stick {
 struct Hanoi {
 	Hanoi(size_t startPiecesNum) : d_sticks{new Stick(startPiecesNum), new Stick, new Stick} {};
 	~Hanoi() { for(size_t i = 0; i < 3; ++i) delete d_sticks.at(i);};
-	std::vector<Stick*> d_sticks{3};
-	std::vector<int>& getStick(size_t i) {return d_sticks.at(i)->d_pcs;}
+	std::vector<int>& getStick(size_t i) {return *(d_sticks.at(i));}
 	void movePiece(size_t src, size_t dest) {
 		getStick(dest).push_back(getStick(src).back());
 		getStick(src).pop_back();
-#ifdef DBG
+#ifdef DISPLAY_STICKS
 		display();
 #endif
 	}
-	void play(size_t src, size_t dest, size_t free, size_t pcs) {
+	void solve(size_t src, size_t dest, size_t free, size_t pcs) {
 		if (pcs == 1) {
 			movePiece(src, dest);
 			return;
 		}
-		play(src, free, dest, pcs - 1);
+		solve(src, free, dest, pcs - 1);
 		movePiece(src, dest);
-		play(free, dest, src, pcs - 1);
+		solve(free, dest, src, pcs - 1);
 	}
-#ifdef DBG
+#ifdef DISPLAY_STICKS
 	void display() {
 		for(size_t i = 0; i < 3; ++i) {
 			d_sticks.at(i)->display();
@@ -54,45 +53,51 @@ struct Hanoi {
 		std::cout << "---------------" << std::endl;
 	}
 #endif
+	private:
+	std::vector<Stick*> d_sticks{};
 };
 
 int main() {
 	{
 		Stick stick;
-		EXPECT((stick.d_pcs.size() == 0));
+		EXPECT((stick.size() == 0));
 	}
 	{
-		Stick stick{5};
-		EXPECT((stick.d_pcs.size() == 5));
-		EXPECT((stick.d_pcs.at(3) == 2));
+		Stick stick(5);
+		EXPECT((stick.size() == 5));
+		EXPECT((stick.at(3) == 2));
 	}
 	{
-		Hanoi  hanoi{5};
-		size_t id{0};
-		EXPECT((hanoi.getStick(id) == std::vector<int>{5,4,3,2,1}));
-		id = 2;
-		EXPECT((hanoi.getStick(id).size() == 0));
+		Hanoi hanoi(5);
+#ifdef DISPLAY_STICKS
+		hanoi.display();
+#endif
+		EXPECT((hanoi.getStick(0) == Stick(5)));
+		EXPECT((hanoi.getStick(2).size() == 0));
 	}
 	{
-		Hanoi  hanoi{2};
-		EXPECT((hanoi.getStick(0) == std::vector<int>{2,1}));
+		Hanoi hanoi{2};
+#ifdef DISPLAY_STICKS
+		hanoi.display();
+#endif
+		EXPECT((hanoi.getStick(0) == Stick(2)));
 		EXPECT((hanoi.getStick(0).size() == 2));
 		hanoi.movePiece(0, 1);
 		EXPECT((hanoi.getStick(0).size() == 1));
 		EXPECT((hanoi.getStick(1).size() == 1));
-		EXPECT((hanoi.getStick(0) == std::vector<int>{2}));
-		EXPECT((hanoi.getStick(1) == std::vector<int>{1}));
+		EXPECT((hanoi.getStick(0).at(0) == 2));
+		EXPECT((hanoi.getStick(1).at(0) == 1));
 	}
 	{
-		Hanoi  hanoi{5};
-		EXPECT((hanoi.getStick(0) == std::vector<int>{5,4,3,2,1}));
-		EXPECT((hanoi.getStick(2).size() == 0));
-#ifdef DBG
+		Hanoi hanoi(5);
+#ifdef DISPLAY_STICKS
 		hanoi.display();
 #endif
-		hanoi.play(0, 2, 1, 5);
+		EXPECT((hanoi.getStick(0) == Stick(5)));
+		EXPECT((hanoi.getStick(2).size() == 0));
+		hanoi.solve(0, 2, 1, 5);
 		EXPECT((hanoi.getStick(0).size() == 0));
-		EXPECT((hanoi.getStick(2) == std::vector<int>{5,4,3,2,1}));
+		EXPECT((hanoi.getStick(2) == Stick(5)));
 	}
 	return 0;
 }
